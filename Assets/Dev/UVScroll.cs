99a29af4217d41fad8cb9cb9f6b14952b1672ad3
m1n1027 <m1n119@icloud.com>
2019-02-01 23:04:03 +0900

    UI周り

--- Assets/Dev/UVScroll.cs
+++ Assets/Dev/UVScroll.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class UVScroll : MonoBehaviour {
 
+    [SerializeField, Range(0.0f, 2.0f)]
+    public float scrollSpeed;
+    public Vector2 scrollPower;
+    private Renderer renderer;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        renderer = GetComponent<Renderer>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        float scroll = Mathf.Repeat(Time.time * 0.8f,1);
-        Vector2 offset = new Vector2(0, scroll);
-        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+
+        float scroll = Mathf.Repeat(Time.time * scrollSpeed,1);
+        Vector2 offset = new Vector2(scrollPower.x * scroll, scrollPower.y * scroll);
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
     }
 }
